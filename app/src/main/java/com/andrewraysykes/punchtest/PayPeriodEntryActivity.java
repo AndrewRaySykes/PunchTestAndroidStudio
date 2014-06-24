1dde6a90a88a666976bed3abb870d148c6af3041
AndrewRaySykes <andrewraysykes@gmail.com>
2014-06-23 22:46:37 -0400

    A little more werk

--- app/src/main/java/com/andrewraysykes/punchtest/PayPeriodEntryActivity.java
+++ app/src/main/java/com/andrewraysykes/punchtest/PayPeriodEntryActivity.java
@@ -1,7 +1,9 @@
 package com.andrewraysykes.punchtest;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
@@ -9,18 +11,44 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import hirondelle.date4j.DateTime;
+
 public class PayPeriodEntryActivity extends Activity {
 
     protected DatePicker mDatePicker;
     protected EditText mEditTextDays;
     protected TextView mTextPreview;
     protected Button mButtonSave;
+    protected SharedPreferences mSharedPreferences;
+
+    protected DateTime mPayPeriodStart;
+    protected DateTime mPayPeriodEnd;
+    protected int mCurrentPayPeriodId;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay_period_entry);
+
+        mDatePicker = (DatePicker) findViewById(R.id.payperiod_datepicker_first_day);
+        mEditTextDays = (EditText) findViewById(R.id.payperiod_edit_days_in_period);
+        mTextPreview = (TextView) findViewById(R.id.payperiod_text_preview_pay_period);
+        mButtonSave = (Button) findViewById(R.id.payperiod_btn_save_pay_period);
+
+        mSharedPreferences = getSharedPreferences(PunchConstants.PREFS_NAME, MODE_PRIVATE);
+
+        CalculateDate();
+
+        mTextPreview.setText("Start:" + mPayPeriodStart + " End: " + mPayPeriodEnd);
+
+
+    }
+
+    private void CalculateDate() {
+        mPayPeriodStart = DateTime.forDateOnly(mDatePicker.getYear(), mDatePicker.getMonth(), mDatePicker.getDayOfMonth());
+        mPayPeriodEnd = mPayPeriodStart.plusDays(Integer.getInteger(mEditTextDays.getText().toString()));
+
     }
 
 
